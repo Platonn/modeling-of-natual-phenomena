fc7423fd01f77acfa8d54770685156ae1ecb72dd
Platonn <platonn@platonn.com>
2017-03-15 00:56:05 +0100

    p3.2 VIDEO WORKING! :D

--- ConnectedSprings.py
+++ ConnectedSprings.py
@@ -10,13 +10,14 @@ class ConnectedSprings:
         self.L = springsParams[:, 1]
         self.m = springsParams[:, 2]
 
-        print('N', self.N)
-        print('k.length', self.k.shape[0])
-        print()
+        # spike:
+        # print('N', self.N)
+        # print('k.length', self.k.shape[0])
+        # print()
 
     def getFunXBis(self):
         def resultFun(j, xj, xj_prev, xj_next):
-            print('resultFun', j, xj, xj_prev, xj_next)
+            # print('resultFun', j, xj, xj_prev, xj_next) #spike
             if j == 0 or j == self.N - 1:
                 return 0  # start and end wall are not accelerating
             else:
@@ -51,15 +52,65 @@ class ConnectedSprings:
 
         for k in range(1, stepsNum):  # from 1, because 0 is t_start - handled above
             for j in range(1, self.N - 1):
-                print('j', j) #spike
+                # print('j', j)  # spike
                 t[k] = t_start + k * h
 
                 xj = x[k - 1][j]
                 xj_prev = x[k - 1][j - 1]
                 xj_next = x[k - 1][j + 1]
 
-                xBis[k][j] = f(j, xj, xj_prev, xj_next)  # xBis zalezy tylko od pozycji 3 ciezarkow w poprzednim kroku: biezacego i 2 sasiadujacych
+                xBis[k][j] = f(j, xj, xj_prev,
+                               xj_next)  # xBis zalezy tylko od pozycji 3 ciezarkow w poprzednim kroku: biezacego i 2 sasiadujacych
                 xPrim[k][j] = xPrim[k - 1][j] + h * xBis[k][j]
                 x[k][j] = x[k - 1][j] + h * xPrim[k][j]
 
         return t, x
+
+    def draw(self, t_start, x_start, xPrim_start, t_end, stepsNum):
+        t, x = self.eulerExplicit2(t_start, x_start, xPrim_start, t_end, stepsNum)
+
+        videoWidth = 640
+        videoHeight = 30
+
+        fps = 100
+
+        video = cv2.VideoWriter(
+            'connectedSprings.avi',
+            cv2.VideoWriter_fourcc(*'MJPG'),
+            fps, (videoWidth, videoHeight))
+
+        for k in range(stepsNum):
+            # prepare cavas:
+            img = np.ones((videoHeight, videoWidth, 3), np.uint8) * 255
+            pos_y = int(videoHeight / 2)
+
+
+            # springs free-state positions:
+            for j in range(self.N):
+                spring_free_pos_x = int(videoWidth * (np.sum(self.L[1:j]) / (np.max(x_start) - np.min(x_start))))
+                cv2.circle(img, (spring_free_pos_x, pos_y), 4, (0, 255, 0), -1)
+
+
+            for j in range(self.N):
+                if (j != 0):
+                    # spring:
+                    spring_left_x = int(videoWidth * (x[k, j - 1] / (np.max(x_start) - np.min(x_start))))
+                    spring_right_x = int(videoWidth * (x[k, j] / (np.max(x_start) - np.min(x_start))))
+
+                    max_k = np.max(self.k[1:])
+                    k_factor = (max_k - self.k[j]+1)
+                    color = (0, 30+k_factor*50, 255)
+
+                    cv2.line(img, (spring_left_x, pos_y), (spring_right_x, pos_y), color, 3)
+
+                # elements:
+                pos_x = int(videoWidth * (x[k, j] / (np.max(x_start) - np.min(x_start))))
+                cv2.circle(img, (pos_x, pos_y), 6, (255, 0, 0), -1)
+
+
+            # cv2.imshow('step ' + str(k), img)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+
+            video.write(img.clip(0, 255).astype(np.uint8))
+        video.release()
--- p3.2.py
+++ p3.2.py
@@ -9,27 +9,31 @@ connectedSprings = ConnectedSprings(np.array([
     # numeracja sprezyn bedzie potem od 1 do N
     [None, None, None],
     [1, 1, 1],
-    [1, 1, 1],
-    [1, 1, 1],
-    [1, 1, None]  # ostatnia sprezyna istnieje(!) - ma swoje k i L
+    [2, 1, 1],
+    [4, 1, 1],
+    [2, 2, None]  # ostatnia sprezyna istnieje(!) - ma swoje k i L
 ]))
 
 # zagadnienie poczatkowe:
-x_start = np.array([0, 1, 2, 3, 4])
-xPrim_start = np.array([0, 0.5, 0, -0.5, 0])
+x_start = np.array([0, 0.5, 2, 2.5, 5])
+xPrim_start = np.array([0, 0, 0, 0, 0])
 t_start = 0
 
 t_end = 20
-stepsNum = 1000
+stepsNum = 2000
 
-t, x = connectedSprings.eulerExplicit2(t_start, x_start, xPrim_start, t_end, stepsNum)
+#spike-debug-old:
+# t, x = connectedSprings.eulerExplicit2(t_start, x_start, xPrim_start, t_end, stepsNum)
+#
+# for j in range(0, connectedSprings.N):
+#     # print('t.shape', t.shape)
+#     # print('x[:][j].shape', x[:, j].shape)
+#     # print('------------')
+#     plt.plot(t, x[:, j], label='x' + str(j))
+#
+# # plt.plot(t, t, label='t') #spike
+# plt.legend()
+# plt.show()
 
-for j in range(0, connectedSprings.N):
-    # print('t.shape', t.shape)
-    # print('x[:][j].shape', x[:, j].shape)
-    # print('------------')
-    plt.plot(t, x[:, j], label='x' + str(j))
-
-# plt.plot(t, t, label='t') #spike
-plt.legend()
-plt.show()
+#spike-new-video:
+connectedSprings.draw(t_start, x_start, xPrim_start, t_end, stepsNum)
